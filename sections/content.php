39230098a31f75e7c4eb2ca180737b971c65a1ef
tommy13911 <136688685+tommy13911@users.noreply.github.com>
2023-06-15 14:56:28 +0300

    Add files via upload

---
+++ sections/content.php
@@ -0,0 +1,25 @@
+<h2>This is the content section</h2>
+
+<?php
+   
+
+    if (isset($_GET['task'])) {
+        $task = filter_var($_GET['task'], FILTER_SANITIZE_NUMBER_INT);        
+
+        
+
+        $filePath = dirname(__FILE__) . "/tasks/task-{$task}.php";
+        if (file_exists($filePath)) {
+            include($filePath);
+        } else {
+            
+            include('tasks/task-no-found.php'); 
+        }
+        
+        
+
+    } else {
+        echo 'Key task is not defined!';
+    }
+
+    
---
+++ sections/header.php
@@ -0,0 +1,9 @@
+<h1>This is the page header!</h1>
+
+<ul>
+    <li><a href="/projectphp/?task=1">Task 01</a></li>
+    <li><a href="/projectphp/?task=2">Task 02</a></li>
+    <li><a href="/projectphp/?task=3">Task 03</a></li>
+    <li><a href="/projectphp/?task=4">Task 04</a></li>
+    <li><a href="/projectphp/?task=5">Task 05</a></li>
+</ul>
